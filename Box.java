aeaf438b74d67f0e8c2bc684dfbf06fd40c1c8d3
louismaillet <louis.maillet@etu.univ-orleans.fr>
2025-02-27 09:28:39 +0100

    Biblio Box: ajout de méthodes pour gérer l'ouverture, la fermeture et la suppression d'objets

--- Box.java
+++ Box.java
@@ -6,8 +6,31 @@ class Thing{
     }
 }
 class Box{
-    ArrayList<Thing> contents = new ArrayList<Thing>();
+    private ArrayList<Thing> contents = new ArrayList<Thing>();
+    private boolean isOpen = false;
+
     public void add(Thing truc){
         this.contents.add(truc);
+    }
+    public boolean contains(Thing truc) {
+        return contents.contains(truc);
+    }
+    public void remove(Thing truc) throws Exception {
+        if (!contents.contains(truc)) {
+            // Si l'objet n'est pas trouvé, on lance une exception
+            throw new Exception("L'objet n'est pas dans la boîte.");
+        }
+        contents.remove(truc);
+    }
+    public void open() {
+        isOpen = true;  
+        System.out.println("La boîte est maintenant ouverte.");
+    }
+    public void close() {
+        isOpen = false;  
+        System.out.println("La boîte est maintenant fermée.");
+    }
+    public boolean isOpen() {
+        return isOpen;
     }
 }
