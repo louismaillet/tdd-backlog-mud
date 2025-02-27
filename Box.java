4c0bd136e33fcfb623f8475bf30d395d091ea8da
louismaillet <louis.maillet@etu.univ-orleans.fr>
2025-02-27 12:01:32 +0100

    Biblio Box: ajout de la gestion de la capacité, des méthodes pour vérifier la présence d'objets, et des tests associés

--- Box.java
+++ Box.java
@@ -1,36 +1,83 @@
 import java.util.ArrayList;
-class Thing{
+
+class Thing {
     String name;
-    public Thing(String name){
+    int volume;
+
+    public Thing(String name) {
         this.name = name;
     }
+
+    public Thing(String name, int volume) {
+        this.name = name;
+        this.volume = volume;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean hasName(String name) {
+        return this.name.equals(name);
+    }
+
+
 }
-class Box{
-    private ArrayList<Thing> contents = new ArrayList<Thing>();
+
+class Box {
+    private ArrayList<Thing> contents = new ArrayList<>();
     private boolean isOpen = false;
+    private int capacity = -1;
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int capacity() {
+        return capacity;
+    }
 
     public void add(Thing truc){
+        
         this.contents.add(truc);
-    }
+        }
+    
+
     public boolean contains(Thing truc) {
         return contents.contains(truc);
     }
-    public void remove(Thing truc) throws Exception {
-        if (!contents.contains(truc)) {
-            // Si l'objet n'est pas trouvé, on lance une exception
-            throw new Exception("L'objet n'est pas dans la boîte.");
-        }
+
+    public void remove(Thing truc)   {
         contents.remove(truc);
-    }
+    }            
+    
     public void open() {
-        isOpen = true;  
+        isOpen = true;
         System.out.println("La boîte est maintenant ouverte.");
     }
+
     public void close() {
-        isOpen = false;  
+        isOpen = false;
         System.out.println("La boîte est maintenant fermée.");
     }
+
     public boolean isOpen() {
         return isOpen;
     }
+
+    public String actionLook() {
+        if (!isOpen) {
+            return "La boîte est fermée.";
+        }
+        String phrase = "La boîte contient : ";
+        for (Thing truc : contents) {
+            phrase += truc.name + ", ";
+        }
+        return phrase;
+    }
+
 }
--- TestsBoxes.java
+++ TestsBoxes.java
@@ -1,14 +1,56 @@
-import org.junit.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class TestsBoxes {
     @Test
     public void testBoxCreate() {
         Box b = new Box();
     }
-/** on veut pouvoir mettre des trucs dedans */
+
     @Test
     public void testBoxAdd(){
         Box b = new Box();
         b.add(new Thing("truc1"));
         b.add(new Thing("truc1"));
+    }
+
+    @Test
+    public void testBoxContains(){
+        Box b = new Box();
+        Thing t = new Thing("truc1");
+        b.add(t);
+        assert(b.contains(t));
+    }
+
+    @Test
+    public void testBoxRemove(){
+        Box b = new Box();
+        Thing t = new Thing("truc1");
+        b.add(t);
+        b.remove(t);
+        assert(b.contains(t));
+    }
+
+    @Test 
+    public void testBoxOpen(){
+        Box b = new Box();
+        b.open();
+        assert(b.isOpen());
+    }
+
+    @Test
+    public void testBoxClose(){
+        Box b = new Box();
+        b.open();
+        b.close();
+        assert(!b.isOpen());
+    }
+    @Test
+    public void testActionLook(){
+        Box b = new Box();
+        Thing t = new Thing("truc1");
+        b.add(t);
+        b.open();
+        assert(b.actionLook().equals("La boîte contient : truc1 "));
     }
 }
